c1f9f89da0faa4211e04d0024bb544f2ae5465a4
wanggaokun <910916015@qq.com>
2020-12-06 20:02:06 +0800

    init

---
+++ km-db/src/main/java/com/km/admin/db/DbAdminApplication.java
@@ -0,0 +1,13 @@
+package com.km.admin.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbAdminApplication.class, args);
+    }
+
+}
---
+++ km-main-admin/src/test/java/com/km/main/admin/KmMainAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.main.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmMainAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
